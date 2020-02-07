21d00180bd0b460fcafbf980263e9b3cc4c886ac
swpanta <swpanta@deerwalk.com>
2020-02-07 17:17:38 +0545

    closer tutorial.

--- grails-app/init/highchart/Application.groovy
+++ grails-app/init/highchart/Application.groovy
@@ -3,7 +3,7 @@ package highchart
 import grails.boot.GrailsApp
 import grails.boot.config.GrailsAutoConfiguration
 
-class Application extends GrailsAutoConfiguration {
+class Application {
     static void main(String[] args) {
 //        GrailsApp.run(Application, args)
         println('-----------test-----------------')
@@ -18,9 +18,28 @@ class Application extends GrailsAutoConfiguration {
         def factor=2
 
         //anonymous function(which has no name)
-        //def printIt={e->println e*factor}
-        list.each {e->println e*factor} // ->(lamda expression) closure
-        list1.each {e->println e*factor} // ->(lamda expression) closure
-        list.each {println "==="+it*factor} //use it only for single parameter
+        def printIt = { e -> println e * factor }
+
+//        list.each printIt // ->(lamda expression) closure //print either list or value
+//        list1.each printIt // ->(lamda expression) closure
+        //list.each {println "==="+it*factor} //use it only for single parameter
+
+        foo({list.each printIt},3)
+        foo({println it},3)
+        println "====closureAtLast=== " //the ways to call closure
+        fooClosureAtLast(3) {println it}
+        fooClosureAtLast(3,{println it})
+        fooClosureAtLast 3, {println it}
+
+        def greet={println "hello $it"}
+        def greetParameter={name->println "hello $name"}
+        greet('swagat')
+        greetParameter('swagat')
+    }
+    def static foo(closure, value) {
+        closure(value * 2)
+    }
+    def static fooClosureAtLast(value,closure) {
+        closure(value * 2)
     }
 }
